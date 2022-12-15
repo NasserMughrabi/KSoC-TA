5808d8ae3e112bde4516e470cf70139e3deb8dac
Nasser Mughrabi <abdalnasseralmughrabi@gmail.com>
2022-12-14 17:08:38 -0700

    assignment requirments done, readme TBD

--- TAApplication/wwwroot/js/Admin/EnrollmentChart.js
+++ TAApplication/wwwroot/js/Admin/EnrollmentChart.js
@@ -1,29 +1,48 @@
 ﻿function getData(startDate, endDate, course) {
-
+    var enrols = [];
     $.get("/Admin/GetData?startDate=" + startDate + "&endDate=" + endDate + "&course=" + course)
-    .done(function (data) {
-        for (var item in data.message) {
-            console.log(data.message[item]);
-        }
+        .done(function (data) {
+            for (var item in data.message) {
+                enrols.push(data.message[item].enrollment)
+            }
+            $("#EnrollmentChart").highcharts().addSeries({
+                name: course,
+                data: enrols
+            });
     });
       
 }
 
 Highcharts.chart('EnrollmentChart', {
     chart: {
-        type: 'spline'
+        type: 'spline',
+        backgroundColor: 'black',
+        style: {
+            color: "white"
+        }
     },
     title: {
-        text: 'Enrollments Over Time'
+        text: 'Enrollments Over Time',
+        style: {
+            color: "white",
+            fontSize: '24px'
+        }
     },
     subtitle: {
-        text: 'University Of Utah'
+        text: 'University Of Utah',
+        style: {
+            color: "white",
+            fontSize: '18px'
+        }
     },
     xAxis: {
         categories: ['Nov 1', '', '', '', '', '', '', 'Nov 8', '', '', ''
             , '', '', '', 'Nov 15', '', '', '', '', '', '', 'Nov 22', '', '', '', '', '', '', 'Nov 29'],
         accessibility: {
             description: 'weeks of the month'
+        },
+        style: {
+            color: "white"
         }
     },
     yAxis: {
@@ -34,11 +53,17 @@ Highcharts.chart('EnrollmentChart', {
             formatter: function () {
                 return this.value + '';
             }
+        },
+        style: {
+            color: "white"
         }
     },
     tooltip: {
         crosshairs: true,
-        shared: true
+        shared: true,
+        style: {
+            color: "white"
+        }
     },
     plotOptions: {
         spline: {
@@ -47,12 +72,14 @@ Highcharts.chart('EnrollmentChart', {
                 lineColor: '#666666',
                 lineWidth: 1
             }
+        },
+        style: {
+            color: "white"
         }
     },
     series: [{
         name: 'CS1400',
         data: [0, 0, 0, 0, 0, 1, 1, 1, 43, 44, 57, 65, 84, 85, 90, 91, 94, 96, 98, 99, 99, 101, 102, 105, 109, 110, 110, 110, 113]
-
     },
     {
         name: 'CS1410',
@@ -61,6 +88,63 @@ Highcharts.chart('EnrollmentChart', {
     {
         name: 'CS2420',
         data: [0, 0, 0, 0, 6, 7, 7, 7, 156, 166, 201, 210, 233, 237, 243, 249, 254, 258, 261, 265, 266, 268, 269, 270, 276, 277, 278, 281, 284]
+    }]
+});
+
+// piechart -------------------------------------------------------------------------------------------------
+
+Highcharts.chart('piechart', {
+    chart: {
+        backgroundColor: 'black',
+        style: {
+            color: "white"
+        },
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Enrollments Over Time',
+        align: 'left',
+        style: {
+            color: "white",
+            fontSize: '24px',
+            textAlign: 'center'
+        },
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'CS1400',
+            y: 65.79,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'CS1410',
+            y: 149.13
+        }, {
+            name: 'CS2420',
+            y: 180.31
+        }]
     }]
 });
 
